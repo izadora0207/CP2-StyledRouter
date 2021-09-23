6886f88048280afe10dae709df88589094fc4220
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2021-09-23 11:13:07 -0300

    base criada

---
+++ src/componente/rodape/Rodape.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Rodape() {
+    return (
+        <footer>
+            <p>
+                <a href=""></a>
+            </p>
+        </footer>
+    )
+}
