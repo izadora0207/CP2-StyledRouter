f82abd0c16caf2a7d86eee9e21248a609bc263a3
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2021-09-24 09:49:57 -0300

    uns bagulhos ali

--- src/sorveteria/componente/corpo/Corpo.jsx
+++ src/sorveteria/componente/corpo/Corpo.jsx
@@ -1,27 +1,67 @@
 import React from 'react'
+import Ft1 from '../../../img/KINDER.jpg'
+import Ft2 from '../../../img/Brisbane.jpg'
+import Ft3 from '../../../img/Kit Kat.jpg'
+import Ft4 from '../../../img/Melbourne.jpg'
+import Ft5 from '../../../img/CascãoTrufado.jpg'
+
+import Ct1 from '../../../img/Cappuccino.jpg'
+import Ct2 from '../../../img/Chocolate Quente.jpg'
+import Ct3 from '../../../img/Expresso.jpg'
+import Ct4 from '../../../img/ExpressoChantilly.jpg'
+import { Li, Ul, Img } from '../../../styled'
 
 export default function Corpo() {
     return (
         <div>
-        <h2>
-            Sabores
-        </h2>
+            <h2>
+                Taças
+            </h2>
+            <Ul>
+                <Li>
+                    <Img src={Ft1} alt="" />
+                    <p>Kinder - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ft2} alt="" />
+                    <p>Brisbane - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ft3} alt="" />
+                    <p>Kit Kat - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ft4} alt="" />
+                    <p>Melbourne - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ft5} alt="" />
+                    <p>CascãoTrufado - 24,99</p>
+                </Li>
+            </Ul>
 
-        <ul>
-            <li>Chocolate</li>
-            <li>Morango</li>
-            <li>Uva</li>
-        </ul>
+            <h2>
+                Acompanhamento
+            </h2>
 
-        <h2>
-            Acompanhamento
-        </h2>
-
-        <ul>
-            <li>Granulado</li>
-            <li>Chantilly</li>
-            <li>Cobertura de Chocolate</li>
-        </ul>
+            <Ul>
+                <Li>
+                    <Img src={Ct1} alt="" />
+                    <p>Kinder - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ct2} alt="" />
+                    <p>Brisbane - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ct3} alt="" />
+                    <p>Kit Kat - 59,99</p>
+                </Li>
+                <Li>
+                    <Img src={Ct4} alt="" />
+                    <p>Melbourne - 59,99</p>
+                </Li>
+            </Ul>
 
         </div>
     )
