7a7242877a1715f4f7db9eaef547cfcd173c78f4
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2021-09-24 08:57:51 -0300

    styled começado

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import ModaMasc from './loja-moda/componente/moda-masc/ModaMasc'
 import ModaFem from "./loja-moda/componente/moda-fem/ModaFem";
 import ModaInfan from "./loja-moda/componente/moda-infan/ModaInfan";
 import Sobre from "./loja-moda/componente/sobre/Sobre";
+import { Div,Body } from './styled'
+import './app.css' //so para nao ficar aquelas 'margins' horriveis de fundo
 
 export default function App() {
   return (
-    <div className="App">
+      <Div>
         <Switch>
-            <Route path="/" exact  component={HomePrincipal}/>
-            <Route path='/sorveteria' component={IndexS}/>
-            <Route path='/loja-roupa' component={IndexL}/>
-            <Route path='/moda-masculina' component={ModaMasc}/>
-            <Route path='/moda-feminina' component={ModaFem}/>
-            <Route path='/moda-infantil' component={ModaInfan}/>
-            <Route path='/sobre' component={Sobre}/>
+          <Route path="/" exact component={HomePrincipal} />
+          <Route path='/sorveteria' component={IndexS} />
+          <Route path='/loja-roupa' component={IndexL} />
+          <Route path='/moda-masculina' component={ModaMasc} />
+          <Route path='/moda-feminina' component={ModaFem} />
+          <Route path='/moda-infantil' component={ModaInfan} />
+          <Route path='/sobre' component={Sobre} />
         </Switch>
-    </div>
+      </Div>
   );
 }
 
