006ff84455f5db7ad025a21697b4bdc3889ee0dc
nathansilva <rm88517@fiap.com.br>
2021-09-24 22:39:46 -0300

    Estrutura moda infantil iniciado

--- src/loja-moda/componente/moda-infan/ModaInfan.jsx
+++ src/loja-moda/componente/moda-infan/ModaInfan.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import bannerinfantil from '../img/bannerInfantil.jpg'
+import roupai1 from '../img/roupa-infantil1.jpg'
+import roupai2 from '../img/roupa-infantil2.jpg'
+import roupai3 from '../img/roupa-infantil3.jpg'
+import roupai4 from '../img/roupa-infantil4.jpg'
+
 
 export default function ModaInfan() {
     return (
-        <>
-        <div>
-            <h1>Moda infantil</h1>
+        <div className='container'>
+            <div>
+                <h1>Moda infantil</h1>
+            </div>
+        <div className='produtos'>
+            <div className="produto-1">
+            </div>
+            <div className="produto-2">
+            </div>
+            <div className="produto-3">
+            </div>
+            <div className="produto-4">
+            </div>
         </div>
-        <section className='produtos'>
-            <div><p>produto 1</p></div>
-            <div><p>produto 2</p></div>
-            <div><p>produto 3</p></div>
-        </section>
-        </>
+        </div>
     )
 }
