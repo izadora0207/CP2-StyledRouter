2d2c04affa96d8da328c37c61f6430815b8f3d76
nathansilva <rm88517@fiap.com.br>
2021-09-24 22:40:35 -0300

    Produtos adicionados

--- src/loja-moda/componente/moda-infan/ModaInfan.jsx
+++ src/loja-moda/componente/moda-infan/ModaInfan.jsx
@@ -16,12 +16,24 @@ export default function ModaInfan() {
             </div>
         <div className='produtos'>
             <div className="produto-1">
+                <img src={roupai1} alt="Camiseta infantil branca Anime" />
+                <p>Camiseta Infantil com Estampa Anime</p>
+                <span>R$29,90</span>
             </div>
             <div className="produto-2">
+                <img src={roupai2} alt="Camisa infantil Jeans" />
+                <p>Camisa Infantil Jeans</p>
+                <span>R$ 69,90</span>
             </div>
             <div className="produto-3">
+                <img src={roupai3} alt="Blusão infantil em moletom gatinha" />
+                <p>Blusão infantil em moletom estampa de gatinha e corações</p>
+                <span>R$49,90</span>
             </div>
             <div className="produto-4">
+                <img src={roupai4} alt="Camiseta infantil laranja Venice" />
+                <p>Camiseta Infantil Venice Laranja</p>
+                <span>R$ 29,90</span>
             </div>
         </div>
         </div>
