14c0f653f707292a44c31ad3bb6c11e7db4c3463
arthursakamoto <arthursakamot@gmail.com>
2021-09-25 22:43:56 -0300

    styled moda masculina iniciada

--- src/loja-moda/componente/moda-masc/ModaMasc.jsx
+++ src/loja-moda/componente/moda-masc/ModaMasc.jsx
@@ -1,55 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import banM from '../img/masculino-banner.jpg'
 import roupa1 from '../img/masculina1.png'
 import roupa2 from '../img/masculina2.png'
 import roupa3 from '../img/masculina3.png'
 import roupa4 from '../img/masculina4.png'
+import { Container, ProdutosAll, ProdutosMasc, InfoMasc, BannerMasc} from './styledMasc'
 
 export default function ModaMasc() {
     return (
-        <div className="container">
-                <h1>Moda masculina</h1>
-                <figcaption className="banner">
-                    <figure>
-                        <img src={banM} alt="Moda Geek"/>
-                    </figure>
-                </figcaption>
-                <div className="produto-1">
+        <Container>
+            <h1>Moda masculina</h1>
+            <div>
+                <BannerMasc img src={banM} alt="Moda Geek"/>
+            </div>
+            <ProdutosAll>
+                <ProdutosMasc>
                     <img src={roupa1} alt="Camisa manga curta em linho bege"/>
-                    <p>
-                        Camisa manga curta em linho com bolso bege
-                    </p>
-                    <span>
-                        R$ 139,90
-                    </span>
-                </div>
-                <div className="produto-2">
+                    <InfoMasc>
+                        <p>Camisa manga curta em linho com bolso bege</p>
+                        <span>R$ 139,90</span>
+                    </InfoMasc>       
+                </ProdutosMasc>
+                <ProdutosMasc>
                     <img src={roupa2} alt="Calça em linho branca"/>
-                    <p>
-                        Calça em linho sem estampa branco
-                    </p>
-                    <span>
-                        R$ 159,90
-                    </span>
-                </div>
-                <div className="produto-3">
+                    <InfoMasc>
+                        <p>Calça em linho sem estampa branco</p>
+                        <span>R$ 159,90</span>
+                    </InfoMasc>
+                </ProdutosMasc>
+                <ProdutosMasc>
                     <img src={roupa3} alt="Camisa manga longa em linho azul"/>
-                    <p>
-                        Camisa manga longa em linho azul
-                    </p>
-                    <span>
-                        R$ 139,90
-                    </span>
-                </div>
-                <div className="produto-4">
+                    <InfoMasc>
+                        <p>Camisa manga longa em linho azul</p>
+                        <span>R$ 139,90</span>
+                    </InfoMasc>
+                </ProdutosMasc>
+                <ProdutosMasc>
                     <img src={roupa4} alt="Camisa manga curta em viscose"/>
-                    <p>
-                        Camisa manga curta em viscose estampa bandana roxo
-                    </p>
-                    <span>
-                        R$ 119,90
-                    </span>
-                </div> 
-        </div>
+                    <InfoMasc>
+                        <p>Camisa manga curta em viscose estampa bandana roxo</p>
+                        <span>R$ 119,90</span>
+                    </InfoMasc>
+                </ProdutosMasc> 
+            </ProdutosAll>          
+        </Container>
     )
 }
---
+++ src/loja-moda/componente/moda-masc/styledMasc.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const ProdutosMasc = styled.div`
+    height: 350px;
+    border-radius: 7px;
+    margin-right: 25px;
+    margin-bottom: 45px;
+
+    img{
+        height: 100%;
+        border-radius: 3px;
+    }
+`;
+
+export const Container = styled.body`
+    background-color: #fff;
+    color: #000;
+`
+
+export const ProdutosAll = styled.div`
+    display: grid;
+    grid-gap: 14px;;
+    max-width: 1280px;
+    margin: 0 auto;
+
+    @media (min-width: 989px){
+        grid-template-columns: repeat(4,minmax(0,1fr));
+    }
+
+    @media (max-width: 988px){
+        grid-template-columns: repeat(2,minmax(0,1fr));
+    }
+`
+export const InfoMasc = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+export const BannerMasc = styled.img`
+    height: 450px;
+    width: 100%;
+    margin-bottom: 25px;
+`
