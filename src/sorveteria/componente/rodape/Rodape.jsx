b8e7f3cc67e905e562e62133b3a61048572d33de
Izadora <RM88494@fiap.com.br>
2021-09-26 17:50:41 -0300

    endereço

--- src/sorveteria/componente/rodape/Rodape.jsx
+++ src/sorveteria/componente/rodape/Rodape.jsx
@@ -5,7 +5,7 @@ export default function Rodape() {
     return (
         <Footer>
             <p>© 2021 - Tage </p>
-            <p>Melhor sorvete não há</p>
+            <p>R.Lessing , 461</p>
         </Footer>
     )
 }
